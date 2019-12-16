f18f665161b972ffd3d8488e534a1ff75fc82d2e
BuildTools <unconfigured@null.spigotmc.org>
2019-12-16 10:55:52 +0200

    Trip, User, Vehicle, Persons CRUD

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role')->default('user');
+            $table->string('status')->default('active');
             $table->rememberToken();
+
+            $table->unsignedBigInteger('created_by');
+            $table->unsignedBigInteger('updated_by');
             $table->timestamps();
         });
     }
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,20 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('users/current', 'UserController@current');
+    Route::put('users/current', 'UserController@updateCurrent');
+    Route::get('vehicles/my', 'VehicleController@currentUserVehicles');
+    Route::apiResources([
+        'trips' => 'TripController',
+        'vehicles' => 'VehicleController',
+        'driver-settings' => 'DriverSettingsController',
+    ]);
+    Route::middleware('allowed_roles:admin')->group(function() {
+        Route::apiResources([
+            'users' => 'UserController',
+        ]);
+    });
 });
+Route::post('users/register', 'UserController@store');
